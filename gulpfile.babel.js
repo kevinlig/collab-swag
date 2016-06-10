ba5e2fd7a68f7770731e2b1e4ddb86ed6792d54a
Kevin Li <kevinlig@gmail.com>
2016-06-10 12:32:15 -0400

    Prevent client side caching when a new release is available

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,8 +9,13 @@ import connect from 'gulp-connect';
 import sass from 'gulp-sass';
 import merge from 'merge-stream';
 import del from 'del';
+import git from 'gulp-git';
+import replace from 'gulp-replace';
 
 let devMode = false
+
+let commitHash = '';
+
 
 gulp.task('clean', () => {
 	return del('./public/**/*');
@@ -88,7 +93,23 @@ gulp.task('minify', ['compile'], () => {
 		.pipe(gulp.dest('./public/js'));
 });
 
-gulp.task('build', ['minify']);
+gulp.task('fetchHash', ['minify'], (done) => {
+	git.revParse({args: '--short HEAD', quiet: true}, (err, hash) => {
+		commitHash = hash;
+		done();
+	});
+});
+
+gulp.task('release', ['fetchHash'], () => {
+	return gulp.src('./public/index.html')
+		// replace the script tag source with a versioned URL
+		.pipe(replace('js/app.js', 'js/app.js?v=' + commitHash))
+		// do the same with CSS
+		.pipe(replace('css/style.css', 'css/style.css?v=' + commitHash))
+		.pipe(gulp.dest('./public'));
+});
+
+gulp.task('build', ['release']);
 
 gulp.task('set-dev', () => {
 	devMode = true;
